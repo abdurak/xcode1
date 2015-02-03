ba37057291ad5827814a63aad792ac19a3a6a595
Abdurrahman Akkas <akkas.a@ic10ddb1bff4ab.nunet.neu.edu>
2015-02-03 14:49:59 -0500

    4

---
+++ tru/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  tru
+//
+//  Created by Abdurrahman Akkas on 2/3/15.
+//  Copyright (c) 2015 tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
