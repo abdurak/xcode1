0aaabd9425f2a8f22624c2622614d524b35e1c4b
Abdurrahman Akkas <akkas.a@ic10ddb1bff4ab.nunet.neu.edu>
2015-02-03 14:35:29 -0500

    Inital commit

---
+++ truTests/truTests.m
@@ -0,0 +1,34 @@
+//
+//  truTests.m
+//  truTests
+//
+//  Created by Abdurrahman Akkas on 2/3/15.
+//  Copyright (c) 2015 tri. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface truTests : XCTestCase
+
+@end
+
+@implementation truTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
